2dd0ba3bb646baf6f3a10a6f841cc2c6082ee24a
KushagraAgarwal525 <kushagraagarwal525.ka@gmail.com>
2022-05-29 20:02:59 +0530

    Stock tracker app

---
+++ src/containers/StockContainer.js
@@ -0,0 +1,25 @@
+import { useState, useEffect, useCallback } from 'react'
+import Stock from "../components/Stock"
+import getPrice from '../services/price'
+
+const StockContainer = ({ stock }) => {
+    const [price, setPrice] = useState(0)
+
+    const updatePrice = useCallback(async () => {
+        const newPrice = await getPrice(stock)
+        if (price !== newPrice) {
+            return setPrice(newPrice)
+        }
+    }, [stock, price])
+
+    useEffect(() => {
+        const interval = setInterval(updatePrice, 1000)
+        return () => clearInterval(interval)
+    }, [updatePrice])
+
+    return (
+        <Stock stock={stock} price={price}/>
+    )
+}
+
+export default StockContainer
